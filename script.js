ac69239cb7535c3194d771c381d00be453cd5cef
sairajeshwar4real <109274862+sairajeshwar4real@users.noreply.github.com>
2022-09-13 21:07:15 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+let saveEl = document.getElementById("save-el")
+let countEl = document.getElementById("count-el")
+let count = 0
+function increment() {
+    count += 1
+    countEl.innerText = count
+}
+count=0
+function save() {
+    let countStr = count + " - "
+    saveEl.innerHTML += countStr
+    console.log(count)
+    count=0
+    countEl.innerText=count
+}
+
+
